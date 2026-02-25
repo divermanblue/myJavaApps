9a5c3e2da21fcf04821279bf31814df1873710c6
Nigel Baker <nigel@coltoncottage.co.uk>
2026-02-25 14:38:56 +0000

    Working version

--- CodeCrack/CodeCrack/src/CodeCrack/Main.java
+++ CodeCrack/CodeCrack/src/CodeCrack/Main.java
@@ -1,29 +1,55 @@
+// Create the package
 package CodeCrack;
+
 // Allow for input setting
+import java.io.*;
 import javax.swing.*;
-// Create class called guess
+
+// Create main class
 public class Main
 {
     // Create the main method
-    public static void main (String[] args)
+    public static void main (String[] args) throws Exception
     {
-        // This string will hold the users input
-        String input;
         //Get the input string
-        input = JOptionPane.showInputDialog("Enter the secret message.");
-        // Test for correctness
-        if (input.equals("Evil Genius"))
+        String userInput = JOptionPane.showInputDialog("1 To set password,\n"+"2 To unlock the message");
+        // This is the file that will be set and opened
+        File file = new File("password.psswrd");
+
+        // Test for opening
+        if(userInput.equals("1"))
         {
-            // User got it right so tell him
-            // System.out.println("You guessed correctly.");
-            JOptionPane.showMessageDialog(null, "You guessed correctly.");
+            // Seeting the password
+            // Get the password
+            String password = JOptionPane.showInputDialog("Enter the password you want to set.");
+            // Write the password to the file
+            FileOutputStream outputStream = new FileOutputStream(file);
+            PrintWriter out = new PrintWriter(outputStream);
+            // Set the password
+            out.println(password);
+            // Close the file
+            out.flush();
+            out.close();
+            outputStream.close();
         }
-        // If the user gets it wrong
         else
         {
-            // Tell him
-            // System.out.println("Wrong.  Try again.");
-            JOptionPane.showMessageDialog(null, "Wrong.  Try again.");
+            // Read the password from the file
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            // This is the password in the file
+            String Password = br.readLine();
+
+            // Get the users guess at the password
+            String userPassword = JOptionPane.showInputDialog("Enter the password to unlock the message.");
+
+            // Check if the password is correct
+            if (userPassword.equals(Password)) {
+                JOptionPane.showMessageDialog(null, "You guessed correctly!");
+            } 
+            else { 
+                JOptionPane.showMessageDialog(null, "Wrong. Try again."); 
+            }              
         }
     }
 }
