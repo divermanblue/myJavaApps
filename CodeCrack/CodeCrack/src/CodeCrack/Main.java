e1148d048669a293df30d80d6ec9afedd31b1457
Nigel Baker <nigel@coltoncottage.co.uk>
2026-02-25 14:12:08 +0000

    Basic version of code

---
+++ CodeCrack/CodeCrack/src/CodeCrack/Main.java
@@ -0,0 +1,29 @@
+package CodeCrack;
+// Allow for input setting
+import javax.swing.*;
+// Create class called guess
+public class Main
+{
+    // Create the main method
+    public static void main (String[] args)
+    {
+        // This string will hold the users input
+        String input;
+        //Get the input string
+        input = JOptionPane.showInputDialog("Enter the secret message.");
+        // Test for correctness
+        if (input.equals("Evil Genius"))
+        {
+            // User got it right so tell him
+            // System.out.println("You guessed correctly.");
+            JOptionPane.showMessageDialog(null, "You guessed correctly.");
+        }
+        // If the user gets it wrong
+        else
+        {
+            // Tell him
+            // System.out.println("Wrong.  Try again.");
+            JOptionPane.showMessageDialog(null, "Wrong.  Try again.");
+        }
+    }
+}
